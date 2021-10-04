50d8c2a8665a0723e855299468516afae66bbc32
Modeesha Kalani <65617018+Modeesha-Kalani@users.noreply.github.com>
2021-10-04 19:20:45 +0530

    request ticket spinner added

--- app/src/main/java/com/example/myapplication/requestticket.java
+++ app/src/main/java/com/example/myapplication/requestticket.java
@@ -7,14 +7,21 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 import android.widget.DatePicker;
-
 import java.util.Calendar;
 
-public class requestticket extends AppCompatActivity
-{
+public class requestticket extends AppCompatActivity implements AdapterView.OnItemSelectedListener
+
+    {
     private DatePickerDialog datePickerDialog;
     private Button dateButton;
+
+    String[] startpoints={"Kandy","Colombo","Matara","Galle","Nuwara-Eliya","Rathnapura","Jaffna"};
+    String[] destinations={"Kandy","Colombo","Matara","Galle","Nuwara-Eliya","Rathnapura","Jaffna"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,7 +31,39 @@ public class requestticket extends AppCompatActivity
         initDatePicker();
         dateButton = findViewById(R.id.datePickerButton);
         dateButton.setText(getTodaysDate());
+
+        Spinner spin1 = (Spinner) findViewById(R.id.startpoint);
+        spin1.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter a1 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,startpoints);
+        a1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin1.setAdapter(a1);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.destination);
+        spin2.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the bank name list
+        ArrayAdapter a2 = new ArrayAdapter(this,android.R.layout.simple_spinner_item,destinations);
+        a2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin1.setAdapter(a2);
+
+
+
     }
+
+        //Performing action onItemSelected and onNothing selected
+        @Override
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+            Toast.makeText(getApplicationContext(), startpoints[position], Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), destinations[position], Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+        }
+
 
     private String getTodaysDate()
     {
@@ -60,6 +99,8 @@ public class requestticket extends AppCompatActivity
         //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
 
     }
+
+
 
     private String makeDateString(int day, int month, int year)
     {
